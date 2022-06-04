8fa32b4b92115f4c6dd8b6329209378c6e0770d5
KiwiIT <77931084+KiwiOnIT@users.noreply.github.com>
2022-06-04 16:34:25 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,46 @@
 <template>
-<head>
-    <meta charset="UTF-8">
-    <title>Random screen generator</title>
-</head>
-<body>
-    <div class="box">
-        <div>
-            <p class="center title">
-                Random screenshot generator
-            </p>
-        </div>
-        <div>
-            <button class="btn center" v-on:click="this.randomScreen()">Click to generate</button>
-        </div>
-        <div>
-            <a class="dynamicLink center link" href="linkFinal" target="_blank" id="linkID"></a>
-        </div>
+<div class="box">
+  <h1 class="title">Passly</h1>
+  <p class="center">A simple password generator !</p>
+    <div>
+      <button value='Generate password'  class="btn" v-on:click="this.generatePassword()">Generate</button>
+      <input type="text"  id="inputText" v-model="password" class="password" readonly>
+      <p> </p>
+      <input type="range" min="8" max="32" value="16" class="slider" id="myRange">
+      <p>Length: <span id="length"></span></p>
     </div>
-</body>
+</div>
+
 </template>
 
 <script>
 export default {
-  methods: {
-    randomScreen() {
-        var lengthChar = 2
-        var lengthNum = 3
-        var lengthLast = 1
-        var char = 'abcdefghijklmnopqrstuvwxyz'
-        var num = '0123456789'
-        var linkChar = ''
-        var linkNum = ''
-        var linkLast = ''
-        for (var i = 0, n = char.length; i < lengthChar; ++i) {
-            linkChar += char.charAt(Math.floor(Math.random() * n));
-        }
-        for (var j = 0, k = num.length; j < lengthNum; ++j) {
-            linkNum += num.charAt(Math.floor(Math.random() * k));
-        }
-        for (var l = 0, m = char.length; l < lengthLast; ++l) {
-            linkLast += char.charAt(Math.floor(Math.random() * m));
-        }
-        var linkFinal = 'https://prnt.sc/' + linkChar + linkNum + linkLast
-        document.getElementById('linkID').innerHTML = linkFinal
 
-        var href = linkFinal;
-        var links = document.getElementsByClassName('dynamicLink');
-        for (var t = 0; t < links.length; t++) {
-            links[t].href = href;
-            links[t].innerHTML = href.replace('http://', "");
-            links[t].style.visibility = 'visible'
+      mounted() {
+        this.generatePassword();
+    },
+    methods: {
+       generatePassword() {
+        var slider = document.getElementById("myRange");
+        var output = document.getElementById("length");
+        output.innerHTML = slider.value;
+
+        slider.oninput = function() {
+            output.innerHTML = this.value;
         }
+        var length = slider.value;
+        var charset = '!@#$%^&*()_-+={[}]|:;"<,>.?/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        var retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+            document.getElementById('inputText').value = retVal 
+        }      
     }
+
   }
 
 }
+
 </script>
 
 <style>
@@ -64,13 +50,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-body {
-  background-color: #2b2b2b;
+.title {
+  margin-top: 120px;
 }
-.box {
+
+.box{
     color: rgb(250, 250, 250);
     width: 800px;
     min-height: 400px;
@@ -82,14 +68,7 @@ body {
     position: absolute;
     left: 50%;
     transform: translate(-50%);
-    margin-top: 240px;
-}
-
-.center {
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    justify-content: center;
+    margin-top: 250px;
 }
 
 .btn {
@@ -103,33 +82,54 @@ body {
     border-radius: 8px;
     cursor: pointer;
     transition-duration: 0.4s;
-
 }
-
 .btn:hover {
-	background-color: rgb(23, 148, 7);
+	background-color: rgb(18, 129, 3);
 }
 
-.link {
-    display: block;
-    background: #4CAF50;
-    padding: 10px;
-    width: 190px;
-    text-align: center;
-    color: #f1f1f1;
-    text-decoration: none;
-    border-radius: 3px;
-    margin-top: 100px;
-    font-size: 20px;
-    transition-duration: 0.4s;
-    visibility: hidden;
+.password {
+  font-size: 20px;
+  margin-left: 10px;
 }
 
-.link:hover {
-    background-color: rgb(23, 148, 7);
+.slider {
+  -webkit-appearance: none;
+  width: 25%;
+  height: 15px;
+  border-radius: 5px;  
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
 }
 
-.title {
-    font-size: 32px;
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%; 
+  background: #4CAF50;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #4CAF50;
+  cursor: pointer;
+}
+
+body {
+  background-color: #2b2b2b;
+}
+
+.center {
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  justify-content: center;
 }
 </style>
